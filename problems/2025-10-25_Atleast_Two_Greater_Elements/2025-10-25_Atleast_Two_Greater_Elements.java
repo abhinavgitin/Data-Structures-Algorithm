3a8f4f7995161c05ca75d275feb297488325416f
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-10-25 23:23:19 +0530

    feat: Add Atleast two greater elements solution (2025-10-25)

---
+++ problems/2025-10-25_Atleast_Two_Greater_Elements/2025-10-25_Atleast_Two_Greater_Elements.java
@@ -0,0 +1,39 @@
+// import java.util.*;
+
+// class Solution {
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         long[] arr = {5, 1, 3, 4, 2};
+//         long[] result = sol.findElements(arr);
+//         for (long num : result) {
+//             System.out.print(num + " ");
+//         }
+//     }
+//     public long[] findElements(long arr[]) {
+//         // Your code goes here
+//         Arrays.sort(arr);
+//         int len = arr.length-2;
+//         long[] nums = new long[len];
+//         for ( int i = 0 ; i < len ; i++ ){
+//             nums[i] = arr[i];
+//         }
+//         return nums;
+//     }
+// }
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        long[] arr = {5, 1, 3, 4, 2};
+        long[] result = sol.findElements(arr);
+        System.out.println(Arrays.toString(result)); // Output: [1, 2, 3]
+    }
+
+    public long[] findElements(long arr[]) {
+        return Arrays.stream(arr)
+                .sorted()
+                .limit(arr.length - 2)
+                .toArray();
+    }
+}
