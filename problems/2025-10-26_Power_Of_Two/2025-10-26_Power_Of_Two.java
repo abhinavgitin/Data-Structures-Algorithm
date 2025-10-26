6e983dab27e75b216f650a7344d84de09f57fd79
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-10-27 03:50:07 +0530

    feat: Add Power Of Two solution (2025-10-26)

---
+++ problems/2025-10-26_Power_Of_Two/2025-10-26_Power_Of_Two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isPowerOfTwo(1));
+        System.out.println(sol.isPowerOfTwo(16));
+        System.out.println(sol.isPowerOfTwo(3)); 
+    }
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && ( n & ( n -1 ) ) == 0;
+    }
+}
