aa49bc76e0efddde61cfd6f2f245596e9f2fc9a7
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-10-27 22:45:25 +0530

    chore: rename java files and classes for debug and run

---
+++ problems/Greatest_Element_Greatest_Occurance_Vise_Versa_2025-10-27/Greatest_Element_Greatest_Occurance_Vise_Versa.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+class Greatest_Element_Greatest_Occurance_Vise_Versa {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 4, 5, 5, 5, 2, 2, 3};
+        Greatest_Element_Greatest_Occurance_Vise_Versa obj = new Greatest_Element_Greatest_Occurance_Vise_Versa();
+        System.out.println("Difference: " + obj.findDiff(arr));
+    }
+    // Function to find the difference between the maximum and minimum frequency of
+    // elements.
+    public int findDiff(int[] arr) {
+        HashMap<Integer,Integer> frequency = new HashMap<>();
+        
+        for (int num : arr) {
+            frequency.put(num, frequency.getOrDefault(num,0) + 1);
+        }
+        
+        int maxFrequency = Integer.MIN_VALUE;
+        int minFrequency = Integer.MAX_VALUE;
+            
+        for (int element : frequency.keySet()) {
+            int count = frequency.get(element);
+            if (count > maxFrequency) {
+                maxFrequency = count;
+            }
+            if (count < minFrequency) {
+                minFrequency = count;
+            }
+        }
+        
+        if (frequency.size() == 1) return 0;
+        
+        return maxFrequency - minFrequency;
+    }
+}
