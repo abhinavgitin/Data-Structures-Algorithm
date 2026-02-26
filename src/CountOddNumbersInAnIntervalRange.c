75e079b4ebc8082efe0bb8177a76236dd8b54060
abhinavgitin <prabhudarsanpuri987@gmail.com>
2026-02-27 00:27:16 +0530

    style: format code for consistency in CountOddNumbersInAnIntervalRange.c

--- src/CountOddNumbersInAnIntervalRange.c
+++ src/CountOddNumbersInAnIntervalRange.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     // take the input
     // i want it to be for int only
     printf("Enter the low and high values: ");
     int low, high;
-    scanf("%d %d",&low,&high);
+    scanf("%d %d", &low, &high);
     int count = high - low + 1;
-    if ( count % 2 == 0 ) {
-        printf("%d",count/2);
-    } else {
-        int addOrNot = ( low % 2 != 0 )? 1 : 0;
-        printf("%d",(count/2)+addOrNot);
+    if (count % 2 == 0)
+    {
+        printf("%d", count / 2);
+    }
+    else
+    {
+        int addOrNot = (low % 2 != 0) ? 1 : 0;
+        printf("%d", (count / 2) + addOrNot);
     }
     return 0;
 }
